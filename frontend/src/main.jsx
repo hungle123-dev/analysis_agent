97ac05c8cff7a8782f0e63ea64f4847b6f2af5bc
hungle123-dev <hunglecrkh2k5@gmail.com>
2026-05-08 07:10:55 +0700

    Use local Monaco assets and loopback API URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { loader } from "@monaco-editor/react";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import "monaco-editor/esm/vs/basic-languages/python/python.contribution";
 import App from "./App.jsx";
 import "./styles.css";
+
+loader.config({ monaco });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
