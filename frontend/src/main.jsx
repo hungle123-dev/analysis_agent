b56d7235da626251536a528b9879c66d2c8a05b5
hungle123-dev <hunglecrkh2k5@gmail.com>
2026-05-08 07:14:29 +0700

    Configure Monaco editor worker

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { loader } from "@monaco-editor/react";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import "monaco-editor/esm/vs/basic-languages/python/python.contribution";
 import App from "./App.jsx";
 import "./styles.css";
+
+self.MonacoEnvironment = {
+  getWorker() {
+    return new EditorWorker();
+  }
+};
 
 loader.config({ monaco });
 
